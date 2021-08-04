6bf86e8826dd0c12a21b059d411cb122f8877dd4
Cengiz YILMAZ <terabilgi@gmail.com>
2021-08-04 19:49:18 +0300

    google-font.js küçültüldü

--- public/js/google-font.js
+++ public/js/google-font.js
@@ -1,11 +1,1 @@
-  WebFontConfig = {
-    google: { families: [ 'Roboto:300,400,500,700,900' ] }
-  };
-  (function() {
-    var wf = document.createElement('script');
-    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-  })();
+  WebFontConfig={google:{families:['Roboto:300,400,500,700,900']}},function(){var a=document.createElement('script'),b;a.src='https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js',a.type='text/javascript',a.async='true',b=document.getElementsByTagName('script')[0],b.parentNode.insertBefore(a,b)}()
